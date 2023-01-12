44d9bd6e66c9bb173af85cce6824f5e06a20bea9
touya watanabe <t-watanabe@un-t.com>
2023-01-12 10:22:40 +0900

    Model追加、LoginController新規作成(処理なし)

---
+++ src/core/app/Http/Controllers/LoginController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use Illuminate\Http\Request;
+
+class LoginController extends AuthenticatedSessionController
+{
+    //
+}
