fe9b4613ab3662d42268acebc289c44aa0d379c8
touya watanabe <t-watanabe@un-t.com>
2023-02-12 00:11:58 +0900

    チーム検索テンプレートをレスポンシブ対応で修正

--- src/core/resources/views/searchTeam/index.blade.php
+++ src/core/resources/views/searchTeam/index.blade.php
@@ -1,93 +1,98 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
+<html>
+  <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-
-<body>
-    <div><button><a href="{{ route('logout') }}">ログアウト</a></button></div>
-    <div class="container">
-        <h3 class="pb-3">招待チーム検索</h3>
-        <div class="container">
-            <h4 class="pb-3">気になる拠点</h4>
-            <div>
-                <form action="" method="get">
-                    <div class="container mb-5">
-                        <div class="form-group row d-flex justify-content-around">
-                            <label for="prefecture" class="col-sm-3 col-form-label">都道府県</label>
-                            <div class="col-sm-7">
-                                <select name="prefecture" class="form-control @error('prefecture') is-invalid @enderror"
-                                    id="prefecture" aria-describedby="nameHelp">
-                                    <option value="">選択してください</option>
-                                    @foreach (\App\Constants\FormConstant::PREFECTURES as $key => $value)
-                                    <option value="{{ $key }}" @if(old('prefecture')==$key || $prefecture==$key)
-                                        selected @endif>{{ $value
-                                        }}
-                                    </option>
-                                    @endforeach
-                                </select>
-                                <small id="prefectureHelp" class="form-text text-muted">検索したいチームの都道府県を選択してください。</small>
-                                @error('prefecture')<div class="invalid-feedback" role="alert"> {{ $message }} </div>
-                                @enderror
-                            </div>
-                        </div>
-                        <div class="form-group row d-flex justify-content-around">
-                            <label for="address" class="col-sm-3 col-form-label">市町村区</label>
-                            <div class="col-sm-7">
-                                <input type="text" name="address" value="{{ old('address')  . $address }}" id="address"
-                                    aria-describedby="nameHelp"
-                                    class="form-control @error('address') is-invalid @enderror" placeholder="例：宮崎市">
-                                <small id="nameHelp" class="form-text text-muted">市町村区を入力してください。</small>
-                                @error('address')<div class="invalid-feedback" role="alert"> {{ $message }} </div>
-                                @enderror
-                            </div>
-                        </div>
-                        <div class="form-group mt-5">
-                            <input type="submit" value="検索する" class="btn btn-primary btn-lg btn-block">
-                        </div>
-                    </div>
-                </form>
-                <div class="container">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>チーム名</th>
-                                <th>チームの拠点</th>
-                                <th>招待リンク</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($teams as $key => $value)
-                            <tr>
-                                <th>{{ $key + 1 }}</th>
-                                <td>{{ $value->team_name }}</td>
-                                <td>{{ \App\Constants\FormConstant::PREFECTURES[$value->prefecture] . $value->address }}
-                                </td>
-                                <td><a href="{{ sprintf(url(__('route_const.consent_link') . " %s"),
-                                        $value->invitation_code)
-                                        }}">{{ sprintf(url(__('route_const.consent_link') . "%s"),
-                                        $value->invitation_code) }}</a>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    <div>
-                        @if (!empty($teams))
-                        {{ $teams->onEachSide(5)->links() }}
-                        @endif
-                    </div>
-                </div>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+      integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+      crossorigin="anonymous"
+    />
+    <title>Search Team</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-light bg-light">
+      <a href="#" class="navbar-brand">Search Teams</a>
+      <div class="navbar-nav">
+        <a href="#" class="nav-item nav-link">トップ</a>
+        <a href="#" class="nav-item nav-link">ログアウト</a>
+      </div>
+    </nav>
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-header">
+              <h3>招待チームを検索</h3>
             </div>
+            <div class="card-body">
+              <form action="{{ route('search.index') }}">
+                @csrf
+                <div class="form-group">
+                  <label for="prefecture">都道府県</label>
+                  <select name="prefecture" class="form-control @error('prefecture') is-invalid @enderror"
+                    id="prefecture" aria-describedby="nameHelp">
+                    <option value="">選択してください</option>
+                    @foreach (\App\Constants\FormConstant::PREFECTURES as $key => $value)
+                    <option value="{{ $key }}" @if(old('prefecture')==$key || $prefecture==$key)
+                        selected @endif>{{ $value
+                        }}
+                    </option>
+                    @endforeach
+                  </select>
+                  <small id="prefectureHelp" class="form-text text-muted">検索したいチームの都道府県を選択してください。</small>
+                  @error('prefecture')<div class="invalid-feedback" role="alert"> {{ $message }} </div>
+                  @enderror
+                </div>
+                <div class="form-group">
+                  <label for="address">住所</label>
+                  <input type="text" name="address" value="{{ old('address')  . $address }}" id="address"
+                  aria-describedby="nameHelp"
+                  class="form-control @error('address') is-invalid @enderror" placeholder="例：宮崎市">
+                  <small id="nameHelp" class="form-text text-muted">市町村区を入力してください。</small>
+                  @error('address')<div class="invalid-feedback" role="alert"> {{ $message }} </div>@enderror
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-</body>
-
+    <div class="container mt-5">
+      <div class="row">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>チーム名</th>
+              <th>チーム活動拠点</th>
+              <th>チームロゴ</th>
+              <th>招待する</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($teams as $key => $value)
+            <tr>
+              <th>{{ $key + 1 }}</th>
+              <td>{{ $value->team_name }}</td>
+              <td>{{ \App\Constants\FormConstant::PREFECTURES[$value->prefecture] . $value->address }}
+              </td>
+              <td></td>
+              <td>
+                <a href="{{ sprintf(url(__('route_const.consent_link') . " %s"),
+                      $value->invitation_code)
+                      }}">招待する
+                </a>
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </body>
 </html>
