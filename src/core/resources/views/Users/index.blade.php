75f53567303b6e936bc6bd566128dabf10179eb1
touya watanabe <t-watanabe@un-t.com>
2023-02-11 23:40:30 +0900

    ログインのテンプレート修正

--- src/core/resources/views/Users/index.blade.php
+++ src/core/resources/views/Users/index.blade.php
@@ -1,66 +1,53 @@
 <!DOCTYPE html>
-<html lang="ja">
-
-<head>
+<html>
+  <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('public/'. 'css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="container">
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">ログイン</div>
-                    <div class="card-body">
-                        <form action="{{ route('login') }}" method="post">
-                            @csrf
-                            <div class="form-group row">
-                                <label class="col-md-4 col-form-label text-md-right" for="email">メールアドレス</label>
-                                <div class="col-md-6">
-                                    <input type="email" name="email"
-                                        class="form-control @error('email') is-invalid @enderror" id="email"
-                                        placeholder="メールアドレス">
-                                    @error('email') <div class="invalid-feedback" role="alert"> {{ $message }} </div>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label class="col-md-4 col-form-label text-md-right" for="password">パスワード</label>
-                                <div class="col-md-6">
-                                    <input type="password" name="password"
-                                        class="form-control @error('password') is-invalid @enderror" id="password"
-                                        required autocomplete="password" placeholder="パスワード">
-                                    @error('password') <div class="invalid-feedback" role="alert"> {{ $message }} </div>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-8 offset-md-4">
-                                    <button class="btn btn-primary ml-3">ログイン</button>
-                                </div>
-                            </div>
-                            @if (session('user.registered')) <div class="alert alert-light" role="alert"> {{
-                                session('user.registered')
-                                }} </div> @endif
-                            <div class="btn-group-vertical float-right">
-                                <a href="{{ route('google.login') }}" class="btn">
-                                    <img src="/public/images/btn_google_signin_dark_normal_web.png">
-                                </a>
-                                <a href="{{ route('line.login') }}">
-                                    <img src="/public/images/btn_login_base.png" alt="" class="btn">
-                                </a>
-                              </div>
-                        </form>
-                    </div>
-                </div>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+      integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+      crossorigin="anonymous"
+    />
+    <title>Login Form</title>
+  </head>
+  <body>
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+        <div class="col-sm-6 col-md-4">
+          <div class="card">
+            <div class="card-header">
+              <h3>Login</h3>
             </div>
+            <div class="card-body">
+              <form action="{{ route('login') }}" method="post">
+                @csrf
+                <div class="form-group">
+                  <label for="email">メールアドレス</label>
+                  <input type="email" name="email"
+                      class="form-control @error('email') is-invalid @enderror" id="email"
+                      placeholder="メールアドレス" aria-describedby="emailHelp">
+                  @error('email')<small id="emailHelp" class="invalid-feedback" role="alert"> {{ $message }} </small> @enderror
+                </div>
+                <div class="form-group">
+                  <label for="password">パスワード</label>
+                  <input type="password" name="password"
+                    class="form-control @error('password') is-invalid @enderror" id="password"
+                    required autocomplete="password" placeholder="パスワード">
+                    @error('password') <small id="passwordHelp" class="invalid-feedback" role="alert"> {{ $message }} </small> @enderror
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">
+                  ログインする
+                </button>
+                <a href="{{ route('tmp_team_user.index') }}" class="btn btn-secondary btn-block mt-3">チームを作成して新規登録</a>
+                <hr />
+                <a href="{{ route('google.login') }}" class="btn btn-block"><img src="/public/images/btn_google_signin_dark_normal_web.png"></a>
+                <a href="{{ route('line.login') }}" class="btn btn-block"><img src="/public/images/btn_login_base.png" alt="" class="btn"></a>
+              </form>
+            </div>
+          </div>
         </div>
-</body>
-
+      </div>
+    </div>
+  </body>
 </html>
