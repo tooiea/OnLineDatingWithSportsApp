dcc9a1dd63cf458093729dc417a334b8deaa352f
tooiea <87057731+tooiea@users.noreply.github.com>
2023-03-08 00:03:00 +0900

    チーム詳細ページの画像サイズ調整、コピーリンクの実装

--- src/core/resources/views/team/detail.blade.php
+++ src/core/resources/views/team/detail.blade.php
@@ -1,43 +1,53 @@
 <!DOCTYPE html>
 <html lang="ja">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link
-      rel="stylesheet"
-      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-      integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-      crossorigin="anonymous"
-    />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <title>Team Detail</title>
-  </head>
-  <body>
-    @include('layouts.nav')
-    <div class="container my-5">
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">チームプロフィール詳細</h3>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <title>Team Detail</title>
+</head>
+
+<body>
+  @include('layouts.nav')
+  <div class="container my-5">
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">チームプロフィール詳細</h3>
+      </div>
+      <div class="card-body">
+        <h5>チーム名:</h5>
+        <p>{{ $myTeam->team->team_name }}</p>
+        <h5>チームの登録人数:</h5>
+        <p>{{ $teamMembersNumber }}人</p>
+        <h5>チームのロゴ画像:</h5>
+        <img
+          src="data:{{ $myTeam->team->image_extension }};base64,{{ base64_encode(file_get_contents($myTeam->team->team_logo)) }}"
+          class="img-fluid" alt="team logo" />
+        <h5>チーム紹介URL:</h5>
+        <p>
+          <a href="{{ $myTeam->team->team_url }}">{{ $myTeam->team->team_url }}</a>
+        </p>
+        <h5>チームへの招待URL:</h5>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" id="myInput"
+            value="{{ sprintf(url(__('route_const.invite_in_team') . '%s'), $myTeam->team->invitation_code) }}">
+          <div class="input-group-append">
+            <button class="btn btn-outline-secondary copy-button" type="button">Copy<i
+                class="fas fa-angle-right ml-2"></i></button>
+          </div>
         </div>
-        <div class="card-body">
-          <h5>チーム名:</h5>
-          <p>{{ $myTeam->team->team_name }}</p>
-          <h5>チームの登録人数:</h5>
-          <p>{{ $teamMembersNumber }}人</p>
-          <h5>チームのロゴ画像:</h5>
-          <img src="data:{{ $myTeam->team->image_extension }};base64,{{ base64_encode(file_get_contents($myTeam->team->team_logo)) }}" alt="team logo" />
-          <h5>チーム紹介URL:</h5>
-          <p>
-            <a href="{{ $myTeam->team->team_url }}">{{ $myTeam->team->team_url }}</a>
-          </p>
-          <h5>チームへの招待URL:</h5>
-          <!-- ボタンを押してコピー機能を追加 -->
-          <p>{{ sprintf(url(__('route_const.invite_in_team') . "%s"), $myTeam->team->invitation_code) }}</p>
-          <!-- TODO 後日実装 -->
-          <!-- <h5>アルバム画像:</h5>
+
+        <div class="alert alert-success fade" role="alert" id="copy-alert">
+          コピーしました
+        </div>
+        <!-- TODO 後日実装 -->
+        <!-- <h5>アルバム画像:</h5>
           <div class="row">
             <div class="col-4">
               <img src="album_image_1.jpg" alt="album image 1" />
@@ -46,9 +56,23 @@
               <img src="album_image_2.jpg" alt="album image 2" />
             </div>
           </div> -->
-        </div>
       </div>
     </div>
-  </body>
-</html>
+  </div>
+  <script>
+    const copyButton = document.querySelector('.copy-button');
+    const input = document.querySelector('#myInput');
+    const alert = document.querySelector('#copy-alert');
 
+    copyButton.addEventListener('click', () => {
+      input.select();
+      document.execCommand('copy');
+      alert.classList.add('show');
+      setTimeout(() => {
+        alert.classList.remove('show');
+      }, 3000);
+    });
+  </script>
+</body>
+
+</html>
