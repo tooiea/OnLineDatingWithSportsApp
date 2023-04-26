ae0adf16b4b5d1b6b1993822b1e3bc905d042c3b
touya watanabe <t-watanabe@un-t.com>
2023-04-26 10:13:50 +0900

    入力補完コード追加(暫定)

--- src/WWW/public/js/ajax.js
+++ src/WWW/public/js/ajax.js
@@ -1,7 +1,6 @@
 $(function () {
 
-    // 「科目」のセレクトボックスが変更された場合
-
+    // 「都道府県」のセレクトボックスが変更された場合
     $('#prefecture').on('change', function () {
         // value属性の値を取得
         let prefecture_val = $(this).val();
@@ -18,19 +17,42 @@ $(function () {
                 prefCode: prefecture_val
             }
         })
-            .done(function (data) {
-                // 初期化
-                $('#city > option').remove();
-                $('#city').append($('<option>').html("選択してください").val(""));
-                $.each(data['result'], function (index, value) {
-                    // コントローラ側で取得した課題のデータをセレクトボックスに追加する
-                    $('#city').append($('<option>').val(value.cityName).text(value.cityName));
-                })
-            })
-            .fail(function () {
-                // 失敗した際の処理を記述
+        .done(function (data) {
+            // 初期化セレクト
+            $('#city > option').remove();
+            $('#city').append($('<option>').html("選択してください").val(""));
+            $.each(data['result'], function (index, value) {
+                // コントローラ側で取得した課題のデータをセレクトボックスに追加する
+                $('#city').append($('<option>').val(value.cityName).text(value.cityName));
             })
 
-    })
+            // 初期化
+            $('#city').val('');
+            $('#city-list').empty();
+            let cities = data['result'].map(function(city) {
+                return city.cityName;
+            });
+            // input要素のイベントを監視して、入力補完を実装する
+            $('#city').on('input', function() {
+                let input_val = $(this).val();
+                let matched_cities = cities.filter(function(city) {
+                    return city.indexOf(input_val) === 0;
+                });
+                $('#city-list').empty();
+                matched_cities.forEach(function(city) {
+                    let li = $('<li>').text(city);
+                    li.on('click', function() {
+                        $('#city').val(city);
+                        $('#city-list').empty();
+                    });
+                    $('#city-list').append(li);
+                });
+            });
+        })
+        .fail(function () {
+            // 失敗した際の処理を記述
+        });
 
-})
+    });
+
+});
