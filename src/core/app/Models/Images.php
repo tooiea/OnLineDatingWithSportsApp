3f853951033e172133da00bdb353a1f8c6144e91
tooiea <87057731+tooiea@users.noreply.github.com>
2023-05-03 17:08:48 +0900

    ファイル保存処理の修正(環境入れ替え時に対応できるように修正)、ファイル名生成の処理をクラスとして定義

--- src/core/app/Models/Images.php
+++ src/core/app/Models/Images.php
@@ -2,8 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * 画像ファイルの保存
+ */
 class Images
 {
+    private const DIRECTORY_OLDWS_TOP = 'oldwds';
+    private const DIRECTORY_OLDWS_LOGO = 'oldwds/images/logo';
+    private const DIRECTORY_OLDWS_ALBUM = 'oldwds/images/album';
+
     /**
      *  画像からMIME TYPEとパスを取得
      *
@@ -12,8 +21,13 @@ class Images
      */
     public static function getImageDetail($image)
     {
-        $tempPath = $image->store('public/upload/images');
-        $storeImage['teamLogo'] = 'public/' . str_replace('public/', 'storage/', $tempPath);
+        // ファイル名を生成
+        $fileName = Files::getFileNameWithUniqueDate($image->guessExtension());
+
+        // 環境ごとに保存ディレクトリを対応(保存後にディレクトリを取得)
+        $savedDir = Storage::putFileAs(self::DIRECTORY_OLDWS_LOGO, $image, $fileName);
+
+        $storeImage['teamLogo'] = $savedDir;
         $storeImage['image_extension'] = $image->getMimeType();
 
         return $storeImage;
