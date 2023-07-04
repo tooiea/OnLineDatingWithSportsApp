bd7b01aa5841a7b7ccf22cd4cf5c3419549ef5e8
touya watanabe <t-watanabe@un-t.com>
2023-07-04 15:33:18 +0900

    csp設定を追加
    ・現状読み込んでいるcss/js/画像に対して指定した形

--- app/app/Http/Middleware/CustomResponseHeader.php
+++ app/app/Http/Middleware/CustomResponseHeader.php
@@ -6,6 +6,9 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * セキュリティ周りのヘッダー設定
+ */
 class CustomResponseHeader
 {
     /**
@@ -15,8 +18,23 @@ class CustomResponseHeader
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $respnse = $next($request);
-        $respnse->header('X-FRAME-OPTIONS', 'DENY');
-        return $respnse;
+        $response = $next($request);
+
+        // クリックジャッキング対策(現状は、iframe/frameは使用していないため全て)
+        $response->header('X-FRAME-OPTIONS', 'DENY');
+
+        // CSP設定
+        // css,img,jsのドメインは現状使用している読み込み先を限定した形で設定(他に追加が必要であれば追加する)
+        $csp = <<<'END'
+        default-src 'self';
+        style-src 'self' 'unsafe-inline' cdn.jsdelivr.net;
+        img-src 'self' data:;
+        script-src 'self' 'unsafe-inline' ajax.googleapis.com cdnjs.cloudflare.com maxcdn.bootstrapcdn.com;
+        END;
+
+        $csp = preg_replace('~(\s)+~u', ' ', trim($csp)); // スペースを整理して1行に
+        $response->headers->set('Content-Security-Policy', $csp);
+
+        return $response;
     }
 }
