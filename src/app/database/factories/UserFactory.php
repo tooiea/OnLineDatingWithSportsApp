29deb646d0ee2d305e6c97224f50576b2941c5b0
渡邊透也 <>
2025-02-28 00:08:14 +0900

    no message

--- src/app/database/factories/UserFactory.php
+++ src/app/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -24,10 +25,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->firstName(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'last_login_at' => CarbonImmutable::now(),
             'remember_token' => Str::random(10),
         ];
     }
