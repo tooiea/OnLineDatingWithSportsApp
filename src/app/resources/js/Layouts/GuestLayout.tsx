46196127b4d9a127c0a3e6405b64450533d6928c
渡邊透也 <tooiea1113@gmail.com>
2025-05-07 23:09:24 +0900

    change: レイアウト調整

--- src/app/resources/js/Layouts/GuestLayout.tsx
+++ src/app/resources/js/Layouts/GuestLayout.tsx
@@ -18,7 +18,7 @@ export default function GuestLayout({ children, routes }: Props) {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
 
-      <main className="flex-grow flex justify-center items-start pt-32 pb-20 sm:pt-20">
+      <main className="flex-grow flex justify-center items-start pt-24 pb-20 sm:pt-32">
         <div className="w-full max-w-md rounded-2xl bg-white px-6 py-6 shadow-md">
           {children}
         </div>
