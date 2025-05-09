5893417eede0b2284a38aafa2e4727a52c55e61c
渡邊透也 <tooiea1113@gmail.com>
2025-05-09 23:47:52 +0900

    clean: 型の厳格化

--- src/app/app/Http/Middleware/ShareInertiaAuthUser.php
+++ src/app/app/Http/Middleware/ShareInertiaAuthUser.php
@@ -1,10 +1,9 @@
 <?php
-
+declare(strict_types=1);
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Symfony\Component\HttpFoundation\Response;
 
